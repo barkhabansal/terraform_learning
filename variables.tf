204b8928831f08fe8b6c37bc7e1ef1192a5be292
Barkha Bansal <barkha.bansal@mastercard.com>
2025-11-20 15:03:07 +0530

    S3 provisioning

--- variables.tf
+++ variables.tf
@@ -8,3 +8,13 @@ variable "bucket_name" {
   description = "Name of the S3 bucket"
   type = string
 }
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type = string
+  sensitive = true
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+  type = string
+  sensitive = true
+}
