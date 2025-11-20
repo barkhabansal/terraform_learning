aaa51225dc391a0b137820ec8e1eef4e2509ab94
Barkha Bansal <barkha.bansal@mastercard.com>
2025-11-20 14:46:59 +0530

    S3 provisioning

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+	required_version = ">= 0.12"
+
+	cloud {
+		organization = "barkha_terraform_learning"
+
+		workspaces {
+			name = "terraform_learning"
+		}
+	}
+
+	required_providers {
+		aws = {
+			source = "hashicorp/aws"
+			version = "~> 5.0"
+		}
+	}
+}
+
+provider "aws" {
+	region = var.aws_region
+}
+
+resource "aws_s3_bucket" "terraform_bucket_resource" {
+	bucket = var.bucket_name
+
+	tags = {
+		Purpose = "Terraform Learning"
+	}
+}
---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "bucket_name" {
+  value = "aws_s3_bucket.terraform_bucket_resource.bucket"
+}
+
+output "bucket_arn" {
+  value = "aws_s3_bucket.terraform_bucket_resource.arn"
+}
